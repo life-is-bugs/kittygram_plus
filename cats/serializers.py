3375154a00bd1bec326c631a7e28e6a13a135030
Mikhail Kelebeev <m@kelebeev.ru>
2023-09-20 19:42:07 +0400

    2

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,35 +1,54 @@
+import datetime as dt
+import webcolors
 from rest_framework import serializers
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True)
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor()
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
         achievements = validated_data.pop('achievements')
         cat = Cat.objects.create(**validated_data)
-
         for achievement in achievements:
             current_achievement, status = Achievement.objects.get_or_create(
-                **achievement
-            )
+                **achievement)
             AchievementCat.objects.create(
-                achievement=current_achievement,
-                cat=cat
-            )
+                achievement=current_achievement, cat=cat)
         return cat
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
